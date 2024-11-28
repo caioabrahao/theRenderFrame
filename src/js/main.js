0b4c78dc6bf4bc872ec46bdeaf14a52bd261e109
Caio Abrahão <contato.caioabrahao@gmail.com>
2024-11-27 22:48:54 -0300

    changes index content. refined a bit

--- src/js/main.js
+++ src/js/main.js
@@ -3,16 +3,32 @@ import ScrollTrigger from 'gsap/ScrollTrigger';
 
 gsap.registerPlugin(ScrollTrigger);
 
+// Title animation
 gsap.to('.hero h1', {
-  scrollTrigger: {
-    trigger: '.hero',
-    start: 'top top',
-    end: '80% top',
-    scrub: true
-  },
-  scale: 0.7,
-  y: -100,
-  rotationX: 10,
-  opacity: 0,
-  filter: 'blur(10px)'
+    scrollTrigger: {
+        trigger: '.hero',
+        start: 'top top',
+        end: '80% top',
+        scrub: true
+    },
+    scale: 0.7,
+    y: -100,
+    rotationX: 10,
+    opacity: 0,
+    filter: 'blur(10px)'
+});
+
+// Subtitle animation with delay
+gsap.to('.hero-subtitle', {
+    scrollTrigger: {
+        trigger: '.hero',
+        start: '20% top', // Starts slightly later
+        end: '80% top',
+        scrub: true
+    },
+    scale: 0.7,
+    y: -50, // Less vertical movement
+    rotationX: 10,
+    opacity: 0,
+    filter: 'blur(5px)' // Slightly less blur
 }); 
